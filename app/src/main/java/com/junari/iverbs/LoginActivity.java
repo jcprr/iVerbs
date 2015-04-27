f4f795c3c1b886295d0a831f144d9b73a2f6030a
jcprr <jcprr@hotmail.com>
2015-04-27 17:40:46 -0500

    se agrega menu y splash

--- app/src/main/java/com/junari/iverbs/LoginActivity.java
+++ app/src/main/java/com/junari/iverbs/LoginActivity.java
@@ -1,10 +1,12 @@
 package com.junari.iverbs;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -12,6 +14,7 @@ import java.util.ArrayList;
 
 public class LoginActivity extends Activity {
 
+    private static final int REQST_CODE = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -69,4 +72,11 @@ public class LoginActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void btnLogin(View view)
+    {
+        startActivityForResult(new Intent(this,MenuActivity.class),REQST_CODE);
+
+    }
+
 }
